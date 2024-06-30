d146fa9f472bd830ef01bdd9d6d774a5102b9398
zainkurnia_ <programmer-fe3@qwertysystem.net>
2024-06-30 15:29:25 +0700

    feat:add new route getPostById

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, Param, Post } from '@nestjs/common';
 
-@Controller('api/posts')
+@Controller('posts')
 export class PostsController {
   @Get()
   getPosts(): string {
     return 'LIST OF POSTS';
+  }
+
+  @Get(':id')
+  @HttpCode(203)
+  @Header('Content-Type', 'application/json')
+  getPostById(@Param('id') id: string): Record<string, string> {
+    return {
+      message: 'Succesfully get post with id:' + id,
+      content: 'Lorem Ipsum!',
+    };
   }
 
   @Post('create-new-post')
